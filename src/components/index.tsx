69fd808b761004ce160edd70beffbaf7b54fa76f
ppyakurel-wyn <ppyakurel@wynshop.com>
2023-01-14 21:11:04 +0545

    main view small update on file directory

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { View } from 'react-native';
+
+type MainViewProps = {};
+
+export const MainView = ({}: MainViewProps) => (
+  <View>
+    <Text>main view</Text>
+  </View>
+);
