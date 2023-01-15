379b7a3686c16f4f3885aef1bc228dac1d6efd79
ppyakurel-wyn <ppyakurel@wynshop.com>
2023-01-15 13:09:06 +0545

    static variable from global constants

---
+++ src/constant/GlobalConstatnt.ts
@@ -0,0 +1,7 @@
+export const GlobalConstants = {
+  HEADING: 'Your Subscription Expired',
+  FOOTER_CONTET:
+    'Your current subscription is expired, Some features are not available.',
+  FOOTER: 'Please contact your service provider.',
+  ACTIVE_OPACITY: 0.8,
+};
